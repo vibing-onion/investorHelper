57ec3267e19cdfe37f273b57f2baedcd3520cd49
Curtis <tcctsang@connect.ust.hk>
2025-08-09 11:33:38 +0800

    docker initial

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+	server: { port: 3000 },
+  preview: { port: 8080 },
   plugins: [react()],
 })
